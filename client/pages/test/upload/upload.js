74298d6de496372c09e6800e6d2624e3e7995e1d
Air <air-lidon@hotmail.com>
2017-08-10 11:28:14 +0700

    upload single image file

--- client/pages/test/upload/upload.js
+++ client/pages/test/upload/upload.js
@@ -1,16 +1,51 @@
 
+// download ===================================
 Template.upload.onCreated(function () {
   Meteor.subscribe('files.images.all');
 });
 
 Template.file.helpers({
   file() {
-    return Images.findOne();
+    return Images.find();
   }
 });
 
-// Template.upload.events({
-//   'click button'() {
-//     Meteor.logout();
-//   },
-// });
+// upload ===================================
+Template.uploadForm.onCreated(function () {
+  this.currentUpload = new ReactiveVar(false);
+});
+
+Template.uploadForm.helpers({
+  currentUpload() {
+    return Template.instance().currentUpload.get();
+  }
+});
+
+Template.uploadForm.events({
+  'change #fileInput'(e, template) {
+    if (e.currentTarget.files && e.currentTarget.files[0]) {
+      // We upload only one file, in case
+      // multiple files were selected
+      const upload = Images.insert({
+        file: e.currentTarget.files[0],
+        streams: 'dynamic',
+        chunkSize: 'dynamic'
+      }, false);
+
+      upload.on('start', function () {
+        template.currentUpload.set(this);
+      });
+
+      upload.on('end', function (error, fileObj) {
+        if (error) {
+          alert('Error during upload: ' + error);
+        } else {
+          alert('File "' + fileObj.name + '" successfully uploaded');
+        }
+        template.currentUpload.set(false);
+      });
+
+      upload.start();
+    }
+  }
+});
