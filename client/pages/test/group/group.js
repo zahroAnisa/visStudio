530d4a5af7cacdf916f0edbe0981150c2e319eef
Air <air-lidon@hotmail.com>
2017-08-15 17:37:21 +0700

    player group ok!

--- client/pages/test/group/group.js
+++ client/pages/test/group/group.js
@@ -11,7 +11,12 @@ Template.playerGroup.onCreated(function () {
 
 Template.playerGroup.helpers({
   players() {
-    return Player.find({}, { sort: { updateAt: 1 } });
+    // return Player.find({}, { sort: { updateAt: 1 } });
+    let group = Session.get("playerGroupOne")
+    let data = Player.find({ playerGroupId: { $nin: [group._id] }}, { sort: { updateAt: 1 } });
+
+    return data
+
   },
   playerGroups() {
     // return PlayerGroup.findOne({"_id": "ANpWEFdcwdqvT3hGL"});
@@ -45,9 +50,59 @@ Template.playerGroup.events({
 
 Template.group.events({
   'click i'(event, template) {   
-    console.log('group.events', this, template.data, this.playerGroupId, this.playerId)
-    Meteor.call('playerGroup.update',this.playerGroupId, this.playerId, (err) => {
+    // console.log('group.events', this, template.data, this.playerGroupId, this.playerId)
+    let data = Session.get("playerGroupOne")
+    let index = data.playerId.indexOf(this.playerId);    // <-- Not supported in <IE9
+    if (index !== -1) {
+      data.playerId.splice(index, 1);
+    }
 
+    let player = Player.findOne({"_id": this.playerId})
+    index = player.playerGroupId.indexOf(this.playerGroupId);    // <-- Not supported in <IE9
+    if (index !== -1) {
+      player.playerGroupId.splice(index, 1);
+    }
+
+    // console.log('group.events', this, template.data, player)
+    Meteor.call('playerGroup.update',this.playerGroupId, {playerId: data.playerId}, (err) => {
+      // Template.playerGroup.__helpers.get('playerGroupOne').call()
+      if(!err) {
+        // $("select").change();
+        // Session.set("playerGroupOne", data );
+        Meteor.call('player.update',this.playerId, {playerGroupId: player.playerGroupId}, (err) => {
+          if(!err) {
+            Session.set("playerGroupOne", data );
+          }
+        })
+      }
+    })
+  }
+});
+
+Template.player.events({
+  'click i'(event, template) {      
+    
+    let data = Session.get("playerGroupOne")
+    if(!data) return 
+    let index = data.playerId.indexOf(this._id);   
+    if (index === -1) {
+      data.playerId.push(this._id)
+    }
+  
+    let player = this
+    index = player.playerGroupId.indexOf(data._id);   
+    if (index === -1) {
+      player.playerGroupId.push(data._id);
+    }
+    // console.log('player.events', this, data, player)
+    Meteor.call('playerGroup.update', data._id, {playerId: data.playerId}, (err) => {
+      if(!err) {
+        Meteor.call('player.update', this._id, {playerGroupId: player.playerGroupId}, (err) => {
+          if(!err) {
+            Session.set("playerGroupOne", data );
+          }
+        })
+      }
     })
   }
 });
