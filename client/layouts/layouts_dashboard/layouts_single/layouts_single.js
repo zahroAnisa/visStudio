c1d6b32d0a85837a3bf1c957b56c029d2f58c1d9
zahroAnisa <anisatuzzahro1997@gmail.com>
2017-09-22 16:07:57 +0700

    player

--- client/layouts/layouts_dashboard/layouts_single/layouts_single.js
+++ client/layouts/layouts_dashboard/layouts_single/layouts_single.js
@@ -3,5 +3,15 @@ Template.layouts_single.rendered = (function (){
   $(document).ready(function(){
     $('ul.tabs').tabs();
   });
+
+  $('.modal').modal({
+      dismissible: true, // Modal can be dismissed by clicking outside of the modal
+      opacity: .5, // Opacity of modal background
+      inDuration: 300, // Transition in duration
+      outDuration: 200, // Transition out duration
+      startingTop: '4%', // Starting top style attribute
+      endingTop: '10%', // Ending top style attribute
+    }
+  );
  
 });
